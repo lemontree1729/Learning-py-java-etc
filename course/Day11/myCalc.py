b77e622680da19344e6f536b0c960b339d518cd8
lemontree1729 <88894223+lemontree1729@users.noreply.github.com>
2021-10-20 16:41:36 +0900

    Add files via upload

---
+++ course/Day11/myCalc.py
@@ -0,0 +1,14 @@
+def plus(a, b):
+    return a + b
+
+def minus(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b:
+        return a / b
+    else:
+        return "INF"
---
+++ course/Day11/mymodule.py
@@ -0,0 +1,6 @@
+from myCalc import plus, minus, multiply, divide
+
+
+a = float(input("First number : "))
+op = input("Operator(+, -, *, /) : ")
+b = float(input("Second number : "))
