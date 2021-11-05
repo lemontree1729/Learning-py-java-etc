b382d52e374c7571aa63c481ea8a4b77c8d92756
rubytree1729 <rubytree1729@gmail.com>
2021-11-05 16:48:17 +0900

    D023 Public

--- course/d020/Quiz12.java
+++ course/d020/Quiz12.java
@@ -2,31 +2,104 @@ package d020;
 
 import java.util.Scanner;
 
-class Quiz12 {
+class Seat {
+    String seatname;
+    String[] stat;
 
-    static void show(String[][] seat, String[] seatcol) {
-
+    Seat(int n, String seatname) {
+        this.seatname = seatname;
+        stat = new String[n];
+        for (int i = 0; i < n; i++) {
+            stat[i] = "---";
+        }
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("system");
+    void set(int n, String name) {
+        stat[n] = name;
+    }
+
+    void show() {
+        System.out.print(seatname + ">>");
+        for (String name : stat)
+            System.out.print(" " + name);
+        System.out.println();
+    }
+}
+
+class SeatSystem {
+    Scanner sc = new Scanner(System.in);
+    Seat[] totalseat = new Seat[3];
+
+    SeatSystem() {
+        System.out.println("system start");
+        totalseat[0] = new Seat(10, "S");
+        totalseat[1] = new Seat(10, "A");
+        totalseat[2] = new Seat(10, "B");
+        run();
+    }
+
+    void run() {
         while (true) {
-            System.out.print(":1, :2, :3, :4");
+            System.out.print("reservation:1, search:2, cancel:3, end:4>>");
             int menunum = sc.nextInt();
-            String[][] chair = new String[3][10];
-            for (int y = 0; y < chair.length; y++) {
-                for (int x = 0; x < chair[0].length; x++) {
-                    chair[y][x] = "---";
-                }
-            }
             switch (menunum) {
             case 1:
-                System.out.print("select seat column S(1), A(2), B(3)>>");
-                int seatcol = sc.nextInt() - 1;
-                
-                show(chair, {seatcol});
+                reserv();
+                break;
+            case 2:
+                search();
+                break;
+            case 3:
+                cancel();
+                break;
+            case 4:
+                end();
+                break;
             }
         }
+    }
+
+    void reserv() {
+        System.out.print("select seat column S(1), A(2), B(3)>>");
+        int seatcol = sc.nextInt() - 1;
+        totalseat[seatcol].show();
+        System.out.print("name>>");
+        String name = sc.next();
+        System.out.print("num>>");
+        int num = sc.nextInt();
+        totalseat[seatcol].set(num, name);
+    }
+
+    void search() {
+        for (Seat seat : totalseat) {
+            seat.show();
+        }
+        System.out.println("<<search ended>>");
+    }
+
+    void cancel() {
+        System.out.print("select seat column S(1), A(2), B(3)>>");
+        int seatcol = sc.nextInt() - 1;
+        Seat target = totalseat[seatcol];
+        target.show();
+        System.out.print("name>>");
+        String name = sc.next();
+        for (int i = 0; i < 10; i++) {
+            if (name.equals(target.stat[i])) {
+                target.set(i, "---");
+                break;
+            }
+        }
+    }
+
+    void end() {
+        System.out.println("<<end the program>>");
+        System.exit(0);
+    }
+}
+
+class Quiz12 {
+    public static void main(String[] args) {
+        SeatSystem my = new SeatSystem();
     }
 }
---
+++ course/d023/README.md
@@ -0,0 +1,25 @@
+# 21/11/05 Python class
+
+## What I learned(Important)
+
+* How to use tkinter ([imagegallery.py](imagegallery.py), [findcookie.py](findcookie.py))
+
+## What I studied myself
+
+* How to make timer
+  * use `time.sleep(sec)`
+    * blocking method
+  * use `.after(ms, function)`
+    * blocking method
+  * use `threading.Timer(sec, function).start()`
+    * non-blocking method
+
+## What I didn't understand
+
+## What I want to know more
+
+## etc(Additional)
+
+## Practice problems
+
+## Useful links
