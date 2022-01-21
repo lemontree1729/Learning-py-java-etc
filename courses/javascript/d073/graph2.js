c8fefd77c4ecee020c684582e8537d6953beb9ae
rubytree1729 <rubytree1729@gmail.com>
2022-01-21 16:48:12 +0900

    d079 work

--- courses/javascript/d073/graph2.js
+++ courses/javascript/d073/graph2.js
@@ -1,8 +1,12 @@
 "use strict";
 const canvas = document.getElementById('graph');
-const dx = 1;
-const maxwidth = parseFloat(window.getComputedStyle(canvas).width);
-const maxheight = parseFloat(window.getComputedStyle(canvas).height);
+const unit = document.createElement('div');
+unit.className = 'dot';
+const dx = 5;
+const maxwidth = parseInt(window.getComputedStyle(canvas).width);
+const maxheight = parseInt(window.getComputedStyle(canvas).height);
+// const unitwidth = parseInt(window.getComputedStyle(unit).width)
+// const unitheight = parseInt(window.getComputedStyle(unit).height)
 function drawPoint(surface, x, y) {
     const pointEl = document.createElement('div');
     pointEl.className = 'dot';
@@ -10,28 +14,28 @@ function drawPoint(surface, x, y) {
     pointEl.style.top = `${maxheight / 2 - y}px`;
     surface.appendChild(pointEl);
 }
-function getPoint(div) {
-    return [parseFloat(div.style.left), parseFloat(div.style.top)];
-}
-function drawGraph(surface, graph, startx = 0, starty = 0) {
+// function getPoint(div: HTMLDivElement) {
+//     return [parseFloat(div.style.left), parseFloat(div.style.top)]
+// }
+function drawGraph(surface, graph) {
     surface.innerHTML = "";
     while (surface.childNodes.length < maxwidth)
-        drawPoint(surface, surface.childNodes.length, graph(surface.childNodes.length + startx) - starty);
+        drawPoint(surface, surface.childNodes.length, graph(surface.childNodes.length));
 }
 function movePoint(div, dx, dy) {
     div.style.left = `${parseFloat(div.style.left) + dx}px`;
     div.style.top = `${parseFloat(div.style.top) + dy}px`;
 }
-function moveGraph(surface, graph, dx = 0, dy = 0) {
-    while (dx-- > 0)
+function moveGraph(surface, graph, dx, offset) {
+    for (let i = 0; i < dx; i++)
         surface.removeChild(surface.firstChild);
-    let x = surface.childNodes.length;
-    surface.childNodes.forEach(v => movePoint(v, dx, dy));
-    // while (x < maxwidth)
-    //     drawPoint(surface, x, graph(surface.childNodes.length))
-    // // graph
+    // surface.removeChild(surface.lastChild!)
+    surface.childNodes.forEach(v => movePoint(v, dx, 0));
+    while (surface.childNodes.length < maxwidth)
+        drawPoint(surface, surface.childNodes.length, graph(surface.childNodes.length + offset));
 }
 let c = 0;
 let myfunction = (x) => Math.sin(x * Math.PI / 180) * 100;
 drawGraph(canvas, myfunction);
-setInterval(() => moveGraph(canvas, myfunction, 1), 10);
+let a = setInterval(() => moveGraph(canvas, myfunction, dx, c += dx), 1000);
+setTimeout(() => clearInterval(a), 5000);
--- courses/javascript/d073/graph2.ts
+++ courses/javascript/d073/graph2.ts
@@ -1,8 +1,12 @@
 const canvas = document.getElementById('graph')!
+const unit = document.createElement('div')
+unit.className = 'dot'
 
-const dx = 1
-const maxwidth = parseFloat(window.getComputedStyle(canvas).width)
-const maxheight = parseFloat(window.getComputedStyle(canvas).height)
+const dx = 5
+const maxwidth = parseInt(window.getComputedStyle(canvas).width)
+const maxheight = parseInt(window.getComputedStyle(canvas).height)
+// const unitwidth = parseInt(window.getComputedStyle(unit).width)
+// const unitheight = parseInt(window.getComputedStyle(unit).height)
 
 function drawPoint(surface: HTMLElement, x: number, y: number) {
     const pointEl = document.createElement('div')
@@ -12,14 +16,14 @@ function drawPoint(surface: HTMLElement, x: number, y: number) {
     surface.appendChild(pointEl)
 }
 
-function getPoint(div: HTMLDivElement) {
-    return [parseFloat(div.style.left), parseFloat(div.style.top)]
-}
+// function getPoint(div: HTMLDivElement) {
+//     return [parseFloat(div.style.left), parseFloat(div.style.top)]
+// }
 
-function drawGraph(surface: HTMLElement, graph: Function, startx = 0, starty = 0) {
+function drawGraph(surface: HTMLElement, graph: Function) {
     surface.innerHTML = ""
     while (surface.childNodes.length < maxwidth)
-        drawPoint(surface, surface.childNodes.length, graph(surface.childNodes.length + startx) - starty)
+        drawPoint(surface, surface.childNodes.length, graph(surface.childNodes.length))
 }
 
 function movePoint(div: HTMLDivElement, dx: number, dy: number) {
@@ -27,17 +31,17 @@ function movePoint(div: HTMLDivElement, dx: number, dy: number) {
     div.style.top = `${parseFloat(div.style.top) + dy}px`
 }
 
-function moveGraph(surface: HTMLElement, graph: Function, dx = 0, dy = 0) {
-    while (dx-- > 0)
+function moveGraph(surface: HTMLElement, graph: Function, dx: number, offset: number) {
+    for (let i = 0; i < dx; i++)
         surface.removeChild(surface.firstChild!)
-    let x = surface.childNodes.length
-    surface.childNodes.forEach(v => movePoint(<HTMLDivElement>v, dx, dy))
-    // while (x < maxwidth)
-    //     drawPoint(surface, x, graph(surface.childNodes.length))
-    // // graph
+    // surface.removeChild(surface.lastChild!)
+    surface.childNodes.forEach(v => movePoint(<HTMLDivElement>v, dx, 0))
+    while (surface.childNodes.length < maxwidth)
+        drawPoint(surface, surface.childNodes.length, graph(surface.childNodes.length + offset))
 }
 
 let c = 0
 let myfunction = (x: number) => Math.sin(x * Math.PI / 180) * 100
 drawGraph(canvas, myfunction)
-setInterval(() => moveGraph(canvas, myfunction, 1), 10)
+let a = setInterval(() => moveGraph(canvas, myfunction, dx, c += dx), 1000)
+setTimeout(() => clearInterval(a), 5000)
